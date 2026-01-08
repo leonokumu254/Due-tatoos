bc2ba340cdc74183594bb418f949f18e2c70c963
leonokumu254 <leonshireku@gmail.com>
2026-01-09 00:40:42 +0300

    remove useState not yet used

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, {  useState } from 'react';
 import { Navbar } from './components/layout/Navbar';
 import { Hero } from './components/sections/Hero';
 import { Gallery } from './components/sections/Gallery';
